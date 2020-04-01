25d4f09683af2a9d329e11ae95d45034e4e56382
Анна Гурова <aagurova_2@edu.hse.ru>
2020-04-01 23:19:35 +0500

    Все задания добавлены в одно решение

---
+++ задание 3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Даны действительные числа х, у. Определить, принадлежит ли точка с координатами х, у заштрихованной части плоскости
+
+(Область ограничена прямыми x=1,x=-1,y=1,y=-1)
+");
+            double x = InsertDouble("Введите значение координаты x: ");
+            double y = InsertDouble("Введите значение координаты y: ");
+
+            if (CheckArea(x, y))
+                Console.WriteLine("Данная точка ВХОДИТ в заданную часть плоскости");
+            else
+                Console.WriteLine("Данная точка НЕ ВХОДИТ в заданную часть плоскости");
+            Console.ReadKey();
+        }
+
+        static bool CheckArea(double x, double y)
+        {
+            return x >= -1 && x <= 1 && y >= -1 && y <= 1;
+        }
+
+        static double InsertDouble(string message)
+        {
+            Console.Write(message);
+            double num;
+            bool ok = true;
+            do
+            {
+                if (!ok)
+                    Console.Write("Вы должны вводить действительные числа! Повторите попытку: ");
+                string temp = Console.ReadLine();
+                ok = Double.TryParse(temp, out num);
+            } while (!ok);
+            return num;
+        }
+    }
+}
---
+++ задание 9/Point.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace задание_9
+{
+     public class Point
+        
+    {
+        public double Value { get; private set; }
+        public Point Next{ get; private set; }
+        public int Count { get; private set; }
+
+        public Point(double value = 0)
+        {
+            this.Value = value;
+            this.Next = null;
+            Count = 1;
+        }
+
+        public void Add(double value)
+        {
+            Point p = new Point(value);
+            Point temp = this;
+            while (temp.Next != null)
+                temp = temp.Next;
+            temp.Next = p;
+            Count++;
+        }
+
+        public override string ToString()
+        {
+            string result = string.Empty;
+            if (this.Next == null)
+                return "Список пустой";
+            else
+            {
+                Point p = this.Next;
+                do
+                {
+                    result += p.Value+ " ";
+                    p = p.Next;
+                } while (p != null);
+                return result;
+            }
+        }
+
+        public double SumNonRec()
+        {
+            Point p = this;
+            double sum = 0;
+            while(p!=null)
+            {
+                sum += p.Value;
+                p = p.Next;
+            }
+            return sum;
+        }
+
+        public static double SumRec(Point p)
+        {
+            if (p == null)
+                return 0;
+            else
+                return p.Value + SumRec(p.Next);
+        }
+
+    }
+}
